ef06a3bd0e5c16159e31152c2884c9afd64f9827
Rodrigo Fazenda Eidelvein <rodrigo.eidelvein@pmweb.com.br>
2018-04-29 15:20:08 -0300

    dev

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
 import reducers from "./reducers";
-import PostsIndex from './components/posts_index';
+import PostsIndex from "./components/posts_index";
+import PostsNew from "./components/posts_new";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex} />
+      <div className="container">
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
